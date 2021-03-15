4a8af785e0632a281aae75850c20590dea7f8f71
wildanbash <wildandawambash@gmail.com>
2021-03-15 14:04:19 +0700

    React Component

---
+++ Modul2/react-component/src/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Header() {
+    return (
+        <code>
+            <h1>
+                Header
+            </h1>
+        </code>
+    )
+}
+export default Header;
