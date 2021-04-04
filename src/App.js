6ef8a02a561069e28f2afcffdb94b2c329a22a32
wildanbash <wildandawambash@gmail.com>
2021-04-04 23:24:37 +0700

    Tugas 2

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import Header from './Header';
+import Navbar from './Navbar';
+import Footer from './Footer';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar/>
+      <Header/>
+      <h2>Hello World</h2>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Footer extends React.Component {
+    render() {
+        return (
+            <div>
+                <p>
+                    Footer
+                </p>
+            </div>
+        );
+    }
+}
+export default Footer;
